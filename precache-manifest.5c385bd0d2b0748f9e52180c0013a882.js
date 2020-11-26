5bda2b81e685d0d71e19a4db5d7840c6606748ae
Karim <cseed322@gmail.com>
2020-11-26 17:54:27 +0300

    Updates

---
+++ precache-manifest.5c385bd0d2b0748f9e52180c0013a882.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3627fe68cfc35a73ff2a9a3e07e4827e",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "77e3a717fe8a8d2d902b",
+    "url": "/react-quiz/static/css/main.ecfd33b1.chunk.css"
+  },
+  {
+    "revision": "15a5515884513ef69a4a",
+    "url": "/react-quiz/static/js/2.a596b71e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-quiz/static/js/2.a596b71e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77e3a717fe8a8d2d902b",
+    "url": "/react-quiz/static/js/main.f158e9f0.chunk.js"
+  },
+  {
+    "revision": "928f8d10e2d93c26f2dd",
+    "url": "/react-quiz/static/js/runtime-main.acabde0b.js"
+  }
+]);
